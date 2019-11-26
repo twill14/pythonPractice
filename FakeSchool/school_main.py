f09d471865b1a7672b6af077f76170fec91c2bfc
DESKTOP-Chicago\twill <twill14@gmail.com>
2019-11-25 19:02:09 -0600

    ummm a lot of stuff

--- FakeSchool/school_main.py
+++ FakeSchool/school_main.py
@@ -1,5 +1,6 @@
 from FakeSchool.student import Student, Frosh
 
+'''
 student1 = Student("Lameena Wright", 1)
 freshmen1 = Frosh("Henford Kaiden", 2)
 
@@ -10,3 +11,15 @@ student1.has_enough_units()
 freshmen1.profile()
 freshmen1.set_units(10)
 freshmen1.units
+
+def char_range(start, stop):
+    for char in range(ord(start), ord(stop) + 1):
+        yield char
+
+
+x = char_range('a', 'z')
+
+for e in x:
+    print(chr(e))
+
+'''
---
+++ palindrom.py
@@ -0,0 +1,28 @@
+import spacy
+
+'''
+string1 = "Bib"
+string2 = "racecar"
+string3 = "Avid diva"
+string4 = "Amen icy cinema"
+string5 = "Shudders"
+
+
+def palindrome(string):
+    string = string.lower().replace(" ", "")
+    for char in string:
+        if char != string[(string.index(char) + 1) * -1]:
+            return False
+    return True
+
+
+print(palindrome(string1))
+print(palindrome(string2))
+print(palindrome(string3))
+print(palindrome(string4))
+print(palindrome(string5))
+'''
+
+string = "Man I wonder if ML is really going to take everyone's jobs. Sure would be sad.  But hopefully by then we'll instill some kind of universal base income!"
+
+
